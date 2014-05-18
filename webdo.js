2ea174116da6e1c280667f1df52ebe69bbe7eaf2
acemtp <acemtp@gmail.com>
2014-05-18 19:08:02 +0000

    first commit

---
+++ webdo.js
@@ -0,0 +1,227 @@
+Gifts = new Meteor.Collection("gifts", {
+  schema: {
+    title: {
+      type: String,
+      label: 'Titre',
+      max: 149
+    },
+    link: {
+      type: String,
+      label: 'Lien',
+      max: 1024
+    },
+    image: {
+      type: String,
+      label: 'Image',
+      max: 1024,
+      optional: true
+    },
+    priority: {
+      type: Number,
+      label: 'Priorité',
+      min: 1,
+      max: 5
+    },
+    ownerId: {
+      type: String,
+      label: 'Owner Id',
+      autoValue: function() {        
+        if (this.isInsert) {
+          return this.userId;
+        } else if (this.isUpsert) {
+          return {$setOnInsert: this.userId};
+        } else {
+          this.unset();
+        }
+      },
+      denyUpdate: true
+    },
+    createdAt: {
+      type: Date,
+      label: 'Date de création',
+      autoValue: function() {
+        if (this.isInsert) {
+          return new Date;
+        } else if (this.isUpsert) {
+          return {$setOnInsert: new Date};
+        } else {
+          this.unset();
+        }
+      },
+      denyUpdate: true
+    },
+    detail: {
+      type: String,
+      label: 'Détail',
+      max: 4096
+    },
+    lockerId: {
+      type: String,
+    },
+    buyerId: {
+      type: String,
+    },
+    archiverId: {
+      type: String,
+    },
+  }
+});
+
+if (Meteor.isClient) {
+//  T9n.language = "fr";
+//  T9n.missingPrefix = ">";
+//  T9n.missingPostfix = "<";
+
+  Accounts.ui.config({passwordSignupFields: 'USERNAME_ONLY'});
+  
+  Router.map(function() {
+    this.route('home', {path: '/'});
+    this.route('createGift');
+    this.route('listGift', {
+      path: '/listGift/:_id',
+      data: function () {
+        return Gifts.find({ownerId: this.params._id, archiverId: {$exists: false}}, {sort: { buyerId: 1, lockerId: 1, priority: -1 }});
+      },
+    });
+
+    this.route('displayGift', {
+      path: '/displayGift/:_id',
+      data: function () {
+        return Gifts.findOne(this.params._id);
+      },
+    });
+    this.route('updateGift', {
+      path: '/updateGift/:_id',
+      data: function () {
+        return Gifts.findOne(this.params._id);
+      },
+    });
+    
+    this.route('listUser');
+  });
+  
+  Router.configure({
+    layoutTemplate: "masterLayout" 
+  })
+
+  Template.home.events({
+    'click input': function () {
+      // template data, if any, is available in 'this'
+      if (typeof console !== 'undefined')
+        console.log("You pressed the button");
+    }
+  });
+
+  AutoForm.hooks({
+    updateGiftForm: {
+      onSuccess: function(operation, result, template) {
+        window.history.back();
+      }
+    }
+  });  
+
+  Template.gift.prio = function() {
+    return _.range(this.priority);
+  };
+  Template.gift.buyedClass = function() {
+    return this.buyerId ? 'buyed' : '';
+  };
+  
+  Template.listUser.users = function() {
+    return Meteor.users.find();
+  };
+  Template.displayGift.prio = function() {
+    return _.range(this.priority);
+  };
+  
+  var findUserNameBy = function (field) {
+    return function () {
+      try {
+        return Meteor.users.findOne(this[field]).profile.name; 
+      } catch (e) {
+        console.log('can not find user ', this[field]);
+        return 'Utilisateur inconnue';
+      }
+    }
+  };
+  
+  Template.displayGift.lockerName = findUserNameBy('lockerId');
+
+  Template.displayGift.buyerName = findUserNameBy('buyerId');
+  var getUpdateObjet = function (doc, field) {
+    var
+    update = {},
+    userId = Meteor.userId(),
+        action, value = {};
+    
+    if (doc[field] === userId) {
+      action = '$unset';
+      value[field] = '';
+    } else {
+      action = '$set';
+      value[field] = userId;
+    }
+    update[action] = value;
+    return update;
+  };
+  
+  Template.displayGift.events({
+    'click .archive': function (e) {
+      e.preventDefault();
+      var update = getUpdateObjet(this, 'archiverId');
+      Gifts.update(this._id, update);
+    },
+    'click .buy': function (e) {
+      e.preventDefault();
+      var update = getUpdateObjet(this, 'buyerId');
+      Gifts.update(this._id, update);
+    },
+    'click .lock': function (e) {
+      e.preventDefault();
+      var update = getUpdateObjet(this, 'lockerId');
+      Gifts.update(this._id, update);
+    }
+  });
+  
+  
+  Template.giftAction.ownerIs = function(currentUser) {
+    return currentUser && this.ownerId === currentUser._id;
+  };
+
+
+  Template.giftAction.events({
+    'click .archive': function (e) {
+      e.preventDefault();
+      Gifts.update(this._id, {$set: {archiverId: Meteor.userId()}});
+    },
+    'click .buy': function (e) {
+      e.preventDefault();
+      Gifts.update(this._id, {$set: {buyerId: Meteor.userId()}});
+    },
+    'click .lock': function (e) {
+      e.preventDefault();
+      Gifts.update(this._id, {$set: {lockerId: Meteor.userId()}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+
+    Accounts.onCreateUser(function(options, user) {
+      
+      if (options.profile)
+        user.profile = options.profile;
+      else
+        user.profile = {name: options.username};
+
+      if (user.services && user.services.google && user.services.google.picture)
+        user.profile.picture = user.services.google.picture;
+      else
+        user.profile.picture = '';
+
+      return user;
+    });
+  
+  });
+}
