17be59e9fc1dd0d89c64fe802ee82e64c6eaf0e0
peernohell <peernohell@gmail.com>
2015-03-05 16:14:01 +0100

    add comments on gift, update dep and rewrite route

--- webdo.js
+++ webdo.js
@@ -1,9 +1,6 @@
-Gifts = new Mongo.Collection("gifts");
+Gifts = new Mongo.Collection('gifts');
+Comments = new Mongo.Collection('comments');
 
-// Hack. simple-form attachSchema seems broken since update of Meteor 1.0.2
-Gifts.attachSchema = Mongo.Collection.prototype.attachSchema;
-Gifts.simpleSchema = Mongo.Collection.prototype.simpleSchema;
-//Mongo.Collection.prototype.attachSchema.call(Gifts, {
 Gifts.attachSchema({
   title: {
     type: String,
@@ -32,13 +29,12 @@ Gifts.attachSchema({
     type: String,
     label: 'Owner Id',
     autoValue: function () {
-      if (this.isInsert) {
+      if (this.isInsert)
         return this.userId;
-      } else if (this.isUpsert) {
+      if (this.isUpsert)
         return { $setOnInsert: this.userId };
-      } else {
-        this.unset();
-      }
+
+      this.unset();
     },
     denyUpdate: true
   },
@@ -46,13 +42,12 @@ Gifts.attachSchema({
     type: Date,
     label: 'Date de création',
     autoValue: function() {
-      if (this.isInsert) {
+      if (this.isInsert)
         return new Date();
-      } else if (this.isUpsert) {
+      if (this.isUpsert)
         return { $setOnInsert: new Date() };
-      } else {
-        this.unset();
-      }
+
+      this.unset();
     },
     denyUpdate: true
   },
@@ -75,8 +70,76 @@ Gifts.attachSchema({
   }
 });
 
+Comments.attachSchema({
+  giftId: {
+    type: String,
+    autoform: {
+      afFieldInput: {
+        type: 'hidden'
+      }
+    }
+  },
+  message: {
+    type: String,
+    max: 1024,
+    autoform: {
+      afFieldInput: {
+        rows: 4
+      }
+    }
+  },
+  createdAt: {
+    type: Date,
+    autoValue: function() {
+      if (this.isInsert)
+        return new Date();
+      if (this.isUpsert)
+        return { $setOnInsert: new Date() };
+
+      this.unset();
+    },
+    denyUpdate: true
+  },
+  author: {
+    type: String,
+    autoValue: function () {
+      var username = Meteor.users.findOne(this.userId).username;
+      if (this.isInsert)
+        return username;
+      if (this.isUpsert)
+        return { $setOnInsert: username };
+
+      this.unset();
+    },
+    denyUpdate: true
+  },
+  authorId: {
+    type: String,
+    autoValue: function () {
+      if (this.isInsert)
+        return this.userId;
+      if (this.isUpsert)
+        return { $setOnInsert: this.userId };
+
+      this.unset();
+    },
+    denyUpdate: true
+  },
+  visible: {
+    type: Boolean,
+    autoform: {
+      afFieldInput: {
+        type: 'hidden'
+      }
+    }
+  },
+  removed: {
+    type: Boolean,
+    defaultValue: false
+  }
+});
 if (Meteor.isClient) {
-  T9n.language = "fr";
+  T9n.language = 'fr';
 //  T9n.missingPrefix = ">";
 //  T9n.missingPostfix = "<";
 
@@ -88,60 +151,18 @@ if (Meteor.isClient) {
     passwordSignupFields: 'USERNAME_ONLY'
   });
 
-  Router.map(function() {
-    this.route('home', {path: '/'});
-    this.route('createGift');
-    this.route('listGift', {
-      path: '/listGift/:_id',
-      data: function () {
-				var showArchived = this.params.query.archived === '1';
-        return {
-          // used to show link to archived or not archived gifts
-          archived: showArchived,
-          // to get the right id for pathFor
-          _id: this.params._id,
-          ownerId: this.params._id,
-					gifts: Gifts.find({
-            ownerId: this.params._id,
-            archived: showArchived
-          }, {
-            sort: { buyerId: 1, lockerId: 1, priority: -1 }
-          })
-        };
-      },
-    });
-
-    this.route('displayGift', {
-      path: '/displayGift/:_id',
-      data: function () {
-        return Gifts.findOne(this.params._id);
-      },
-    });
-    this.route('updateGift', {
-      path: '/updateGift/:_id',
-      data: function () {
-        return Gifts.findOne(this.params._id);
-      },
-    });
-
-    this.route('listUser');
-  });
-
-  Router.configure({
-    layoutTemplate: "masterLayout"
-  });
 
   Template.home.events({
     'click input': function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+        console.log('You pressed the button');
     }
   });
 
-	Template.listGift.helpers({
-    listGiftArchived: function () {
-      return ('listGift', { _id: this.ownerId }, { query: 'archived=1' });
+	Template.userGifts.helpers({
+    userGiftsArchived: function () {
+      return Router.go('userGifts', { _id: this.ownerId }, { query: 'archived=1' });
     }
   });
 
@@ -150,7 +171,6 @@ if (Meteor.isClient) {
       return currentUser && this.ownerId === currentUser._id;
     }
   });
-
 
   Template.giftAction.events({
     'click .archive': function (e) {
@@ -167,19 +187,37 @@ if (Meteor.isClient) {
     },
     'click .unarchive': function (e) {
       e.preventDefault();
-      Gifts.update(this._id, {$unset: {archived: false, lockerId: "", buyerId: ""}});
+      Gifts.update(this._id, {$unset: {archived: false, lockerId: '', buyerId: ''}});
     }
   });
 
   AutoForm.hooks({
     insertGiftForm: {
-       onSuccess: function(operation, result, template) {
+      onSuccess: function() {
         window.history.back();
       }
     },
     updateGiftForm: {
-      onSuccess: function(operation, result, template) {
+      onSuccess: function() {
         window.history.back();
+      }
+    },
+    insertPrivateComment: {
+      before: {
+        insert: function (doc) {
+          console.log('before insert', arguments);
+          return doc;
+        }
+      },
+      after: {
+        insert: function (doc) {
+          console.log('After insert', arguments);
+          return doc;
+        }
+      },
+      onSubmit: function() {
+        console.log('on submit', arguments);
+        this.done();
       }
     }
   });
@@ -193,10 +231,8 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.listUser.helpers({
-    users: function() {
-      return Meteor.users.find();
-    }
+  Template.users.helpers({
+    users: Meteor.users.find.bind(Meteor.users)
   });
 
   var findUserNameBy = function (field) {
@@ -210,14 +246,24 @@ if (Meteor.isClient) {
     };
   };
 
-  Template.displayGift.helpers({
+  Template.giftShow.helpers({
     prio: function() {
       return _.range(this.priority);
     },
     lockerName: findUserNameBy('lockerId'),
     buyerName: findUserNameBy('buyerId'),
+    ownerName: findUserNameBy('ownerId'),
     ownerIs: function(currentUser) {
       return currentUser && this.ownerId === currentUser._id;
+    },
+    ownerIsNot: function(currentUser) {
+      return currentUser && this.ownerId !== currentUser._id;
+    },
+    publicComments: function () {
+      return Comments.find({ visible: true }).fetch();
+    },
+    privateComments: function () {
+      return Comments.find({ visible: false }).fetch();
     }
   });
 
@@ -239,7 +285,7 @@ if (Meteor.isClient) {
     return update;
   };
 
-  Template.displayGift.events({
+  Template.giftShow.events({
     'click .archive': function (e) {
       e.preventDefault();
       // TODO rework archiverId => archived
@@ -260,31 +306,111 @@ if (Meteor.isClient) {
 
   UI.registerHelper('priorities', function() {
     return [
-        { label: "5 étoiles - Doit avoir", value: 5 },
-        { label: "4 étoiles - Adorerais avoir", value: 4 },
-        { label: "3 étoiles - Aimerais avoir", value: 3 },
-        { label: "2 étoiles - J'y pense", value: 2 }
+      { label: '5 étoiles - Doit avoir', value: 5 },
+      { label: '4 étoiles - Adorerais avoir', value: 4 },
+      { label: '3 étoiles - Aimerais avoir', value: 3 },
+      { label: "2 étoiles - J'y pense", value: 2 }
     ];
   });
 }
+function onStartup () {
+  Meteor.publish('users', Meteor.users.find.bind(Meteor.users));
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-
-    Accounts.onCreateUser(function(options, user) {
-
-      if (options.profile)
-        user.profile = options.profile;
-      else
-        user.profile = {name: options.username};
-
-      if (user.services && user.services.google && user.services.google.picture)
-        user.profile.picture = user.services.google.picture;
-      else
-        user.profile.picture = '';
-
-      return user;
-    });
-
+  Meteor.publish('user.gifts', function (userId) {
+    check(userId, String);
+    return Gifts.find({ ownerId: userId });
   });
+  Meteor.publish('gift.show', function (giftId) {
+    check(giftId, String);
+    var gift = Gifts.findOne(giftId);
+    // array of user ids needed to show the gift
+    var userIds = [ gift.ownerId, gift.buyerId, gift.lockerId, gift.ownerId ];
+    var isPersonalGift = gift.ownerId === this.userId;
+
+    var commentSelector = {giftId: giftId, removed: false};
+    if (isPersonalGift)
+      commentSelector.visible = true;
+
+    return [
+      Meteor.users.find({ _id: {$in: userIds}}),
+      Gifts.find({ _id: giftId }),
+      Comments.find(commentSelector)
+    ];
+  });
+
+  Accounts.onCreateUser(function(options, user) {
+
+    if (options.profile)
+      user.profile = options.profile;
+    else
+      user.profile = {name: options.username};
+
+    if (user.services && user.services.google && user.services.google.picture)
+      user.profile.picture = user.services.google.picture;
+    else
+      user.profile.picture = '';
+
+    return user;
+  });
+
 }
+if (Meteor.isServer)
+  Meteor.startup(onStartup);
+
+
+// routes
+Router.route('/', {name: 'home'});
+Router.route('/user/:_id/gift/create', {name: 'gift.create'});
+Router.route('/user/:_id/gifts', {
+  name: 'user.gifts',
+  waitOn: function () {
+    return Meteor.subscribe('user.gifts', this.params._id);
+  },
+  data: function () {
+    var showArchived = this.params.query.archived === '1';
+    return {
+      // used to show link to archived or not archived gifts
+      archived: showArchived,
+      // to get the right id for pathFor
+      _id: this.params._id,
+      // TODO remove ownerId from tempalte and use _id
+      ownerId: this.params._id,
+      gifts: Gifts.find({
+        ownerId: this.params._id,
+        archived: showArchived
+      }, {
+        sort: { buyerId: 1, lockerId: 1, priority: -1 }
+      })
+    };
+  }
+});
+
+Router.route('/gift/:_id', {
+  name: 'gift.show',
+  waitOn: function () {
+    return Meteor.subscribe('gift.show', this.params._id);
+  },
+  data: function () {
+    var gift = Gifts.findOne(this.params._id);
+    if (gift)
+      gift.comments = Comments.find({giftId: this.params._id});
+
+    return gift;
+  }
+});
+Router.route('/gift/:_id/update', {
+  name: 'gift.update',
+  data: function () {
+    return Gifts.findOne(this.params._id);
+  }
+});
+
+Router.route('/users', {
+  waitOn: function () {
+    return Meteor.subscribe('users');
+  }
+});
+
+Router.configure({
+  layoutTemplate: 'masterLayout'
+});
