f492ed81bb8310105d1b3e989e2b2c32756b0179
Francois Lecroart <francois@e-founders.com>
2016-02-01 22:57:26 +0100

    fix lint for server/main + add lib ecmascript

--- server/main.js
+++ server/main.js
@@ -1,106 +1,106 @@
+function onStartup() {
+  Meteor.publish('users', function publishUsers() {
+    const user = Meteor.users.findOne(this.userId, { fields: { 'profile.friends': 1 } });
+    if (!user) return this.ready();
 
-function onStartup () {
-  Meteor.publish('users', function () {
-    var userProfile = (Meteor.users.findOne(this.userId, { fields: { 'profile.friends': 1 } }) || {}).profile || {};
     return Meteor.users.find({
-      _id: { '$in': userProfile.friends || [] }
+      _id: { $in: user.profile.friends || [] },
     });
   });
 
-  Meteor.publish('user.profile', function () {
+  Meteor.publish('user.profile', function publishUserProfile() {
     return Meteor.users.find({ _id: this.userId }, { limit: 1 });
   });
 
-  Meteor.publish('user.gifts', function (userId) {
+  Meteor.publish('user.gifts', function pubishUserGifts(userId) {
     check(userId, String);
-    var request = { ownerId: userId };
-    var options = {};
+    const request = { ownerId: userId };
+    const options = {};
     if (this.userId === userId) {
       request.suggested = false;
-      options.fields = { 'lockerId': 0, 'buyerId': 0 };
+      options.fields = { lockerId: 0, buyerId: 0 };
     }
 
-    return [ Gifts.find(request, options), Meteor.users.find({ _id: userId }) ];
+    return [Gifts.find(request, options), Meteor.users.find({ _id: userId })];
   });
 
-  Meteor.publish('gifts.tobuy', function () {
+  Meteor.publish('gifts.tobuy', function publishGiftsTobuy() {
     return Gifts.find({ archived: false, lockerId: this.userId, buyerId: null });
   });
 
-  Meteor.publish('gifts.buyed', function () {
+  Meteor.publish('gifts.buyed', function publishGiftsBuyed() {
     return Gifts.find({ archived: false, buyerId: this.userId });
   });
 
-  Meteor.publish('gifts.latest', function () {
+  Meteor.publish('gifts.latest', function publishGiftsLatest() {
     if (!this.userId) return this.ready();
 
-    var friends = Meteor.users.findOne(this.userId).profile.friends || [];
-    var userIdx = friends.indexOf(this.userId);
-    if (userIdx >= 0)
-      friends.splice(userIdx, 1);
-    return Gifts.find({ archived: false, ownerId: { $in: friends } }, { limit: 10, sort: { createdAt: -1 } });
+    const friends = Meteor.users.findOne(this.userId).profile.friends || [];
+    const userIdx = friends.indexOf(this.userId);
+    if (userIdx >= 0) friends.splice(userIdx, 1);
+
+    const selector = { archived: false, ownerId: { $in: friends } };
+    const options = { limit: 10, sort: { createdAt: -1 } };
+
+    return Gifts.find(selector, options);
   });
 
-  Meteor.publish('gift.show', function (giftId) {
+  Meteor.publish('gift.show', function publishGiftShow(giftId) {
     check(giftId, String);
-    var gift = Gifts.findOne({ _id: giftId });
-    var options = { limit: 1 };
+    const gift = Gifts.findOne({ _id: giftId });
+    const options = { limit: 1 };
 
     if (gift.ownerId === this.userId) {
       if (gift.suggested) return this.ready();
-      options.fields = { 'lockerId': 0, 'buyerId': 0 };
+      options.fields = { lockerId: 0, buyerId: 0 };
     }
     return Gifts.find({ _id: giftId }, options);
   });
 
-  Meteor.publish('gift.comments', function (giftId) {
+  Meteor.publish('gift.comments', function publishGiftComments(giftId) {
     check(giftId, String);
-    var isPersonalGift = !!Gifts.findOne({ _id: giftId, ownerId: this.userId });
+    const isPersonalGift = !!Gifts.findOne({ _id: giftId, ownerId: this.userId });
 
-    var commentSelector = { giftId: giftId, removed: false };
-    if (isPersonalGift)
-      commentSelector.visible = true;
+    const commentSelector = { giftId, removed: false };
+    if (isPersonalGift) commentSelector.visible = true;
 
     return Comments.find(commentSelector);
   });
 
-  Accounts.onCreateUser(function(options, user) {
+  Accounts.onCreateUser((options, user) => {
+    const newUser = Object.assign({}, user);
 
-    if (options.profile)
-      user.profile = options.profile;
-    else
-      user.profile = {name: options.username};
+    newUser.profile = options.profile ? options.profile : { name: options.username };
 
     if (user.services && user.services.google && user.services.google.picture)
-      user.profile.avatar = user.services.google.picture;
+      newUser.profile.avatar = user.services.google.picture;
     else
-      user.profile.avatar = '';
+      newUser.profile.avatar = '';
 
-    return user;
+    return newUser;
   });
-
 }
 
-function extractMeta (url) {
+function extractMeta(url) {
   try {
-    var result = HTTP.call('GET', url);
-    var m;
-    var meta = {};
+    const result = HTTP.call('GET', url);
+    const meta = {};
+    let m;
 
-    if(result.statusCode !== 200) {
+    if (result.statusCode !== 200) {
       console.log('bad status code', result.statusCode);
       return undefined;
     }
 
-    var re = /<meta.*?(?:name|property)=['"](.*?)['"].*?content=['"](.*?)['"].*?>/gmi;
+    let re = /<meta.*?(?:name|property)=['"](.*?)['"].*?content=['"](.*?)['"].*?>/gmi;
     while ((m = re.exec(result.content)) !== null) {
       if (m.index === re.lastIndex)
         re.lastIndex++;
 
 //        console.log('m', m[1], m[2]);
-      if(m[1] === 'description' || m[1] === 'og:description' || m[1] === 'twitter:description') meta.description = m[2];
-      if(m[1] === 'og:image' || m[1] === 'twitter:image') meta.image = m[2];
-      if(m[1] === 'og:title' || m[1] === 'twitter:title') meta.name = m[2];
+      if (m[1] === 'description' || m[1] === 'og:description' || m[1] === 'twitter:description') meta.description = m[2];
+      if (m[1] === 'og:image' || m[1] === 'twitter:image') meta.image = m[2];
+      if (m[1] === 'og:title' || m[1] === 'twitter:title') meta.name = m[2];
     }
 
     re = /<title>(.*)<\/title>/gmi;
@@ -123,11 +123,11 @@ function extractMeta (url) {
 Meteor.startup(onStartup);
 
 Meteor.methods({
-  curExtractMeta: function(url) {
+  curExtractMeta(url) {
     check(url, String);
     this.unblock();
-    var meta = extractMeta(url);
-    if(!meta) throw new Meteor.Error('not meta extracted');
+    const meta = extractMeta(url);
+    if (!meta) throw new Meteor.Error('not meta extracted');
     return meta;
-  }
+  },
 });
